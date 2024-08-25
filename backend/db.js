a1661f97a6d87eb258ebc996230d5748714afba1
siddarthakvs098 <siddarthakvs2001@gmail.com>
2024-08-25 11:32:33 +0530

    removed db

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 
 const mongoose = require("mongoose");
-// mongoose.connect("mongodb+srv://MrRobot:100xdevs.com@cluster0.vwojkhm.mongodb.net/Todo-App");
 
 const todoSchema = mongoose.Schema({
     title:String,
